264338978dc1ef4ff7da18d955f938ec7be9a9f0
ShantMahserejian <shant.mahserejian@pnnl.gov>
2024-05-14 19:00:26 -0700

    Removed fba_utils.py and replaced with updated functions into a new file, bmca_utils.py with corresponding unit tests

--- src/bmca_utils.py
+++ src/bmca_utils.py
@@ -1,59 +1,60 @@
-import sys
+"""Utils module for bmca related processing."""
+
+from typing import Tuple
 
 import cobra
-import gurobipy
 import numpy as np
 import pandas as pd
 from cobra import Gene, Reaction
-from eflux2 import EFlux2
 
 
-def get_flux_bounds(model, rxns_of_interest, zero_threshold=1e-9):
+def get_max_flux_bounds(
+    model: cobra.Model, rxn_list: list[str], precision: int = 9
+) -> Tuple[cobra.Model, pd.DataFrame]:
     """Get flux bounds from FVA to use in surrogate model of reference strain.
 
-    The model is optimized to get fluxes for reactions of interest and runs FVA to get flux bounds adjusted by FVA for all other reactions.
+    Note: FVA = flux variability analysis
     inputs:
         model: cobra model
-        rxns_of_interest: list of reactions of interest, corresponding to reference strain selection criteria
+        rxn_list: list of reactions of interest, corresponding to reference strain selection criteria
         zero_threshold: magnitude threshold to identify and replace numerically zero flux values
     outputs:
-        flux_bounds: flux min and max values to be used as a representative bounds of the reference strain.
+        max_flux_bounds: max flux values to be used as a representative bounds of the reference strain.
     """
-    # Get optimized fluxes
-    opt_df = model.optimize().to_frame()
-
-    # Set bounds for reactions of interest using optimized values
-    for rxn in rxns_of_interest:
-       model.reactions.get_by_id(rxn).lower_bound = opt_df.loc[rxn, 'fluxes']
-
     # Run FVA to get (reasonably) tight bounds for all other reactions
-    keep_rxn_list = [r.id for r in model.reactions if (r.id not in rxns_of_interest)]
-    flux_bounds = cobra.flux_analysis.flux_variability_analysis(model=model, reaction_list=keep_rxn_list, 
-                                                               fraction_of_optimum=0.85, processes=8)
-    for c in flux_bounds.columns:
-        for r in flux_bounds.index:
-            if (flux_bounds[c][r] > -1*zero_threshold) and (flux_bounds[c][r] < zero_threshold):
-                flux_bounds[c][r] = 0
+    keep_rxn_list = [r.id for r in model.reactions if (r.id not in rxn_list)]
+    flux_bounds = cobra.flux_analysis.flux_variability_analysis(
+        model=model, reaction_list=keep_rxn_list, fraction_of_optimum=0.85, processes=8
+    )
+    max_flux_bounds = flux_bounds["maximum"].round(decimals=precision).to_dict()
 
-    return model, flux_bounds
+    return max_flux_bounds
 
 
-# Function to create dictionary of reactions to isozyme sets (corresponding genes from gene reaction rules)
-def get_gpr_dict(model):
-    """Returns the gene reaction rule (GPR) for each reaction in the model."""
+def get_gpr_dict(model: cobra.Model) -> dict[Reaction, list[list[Gene]]]:
+    """Gene reaction rule (GPR) for each reaction in the model.
+
+    inputs:
+        model: cobra model
+    outputs:
+        gpr_dict: dictionary of reactions to isozyme sets (corresponding genes from gene reaction rules)
+    """
     # Parse GPR into a dict containing isozymes (separated by 'or')
     # Each isozyme has a set of subunits (separated by 'and')
-    gpr_dict = dict()
+    gpr_dict = {}
     for r in model.reactions:
         if r.gene_reaction_rule:
             isozymes = set()
-            for isozyme in [isozyme.strip('() ') for isozyme in r.gene_reaction_rule.split(' or ')]:
-                isozymes.add(frozenset(gene.strip('() ') for gene in isozyme.split(' and ')))
+            for isozyme in [isozyme.strip("() ") for isozyme in r.gene_reaction_rule.split(" or ")]:
+                isozymes.add(frozenset(gene.strip("() ") for gene in isozyme.split(" and ")))
             gpr_dict[r] = isozymes
 
     return gpr_dict
 
-def gene_expression_to_enzyme_activity(model, gpr: dict[Reaction, list[list[Gene]]], expression: dict[Gene, float]):
+
+def gene_expression_to_enzyme_activity(
+    model: cobra.Model, gpr: dict[Reaction, list[list[Gene]]], expression: dict[Gene, float]
+) -> dict[Reaction, float]:
     """Map gene expression to enzyme activity inputs.
 
     inputs:
@@ -70,7 +71,7 @@ def gene_expression_to_enzyme_activity(model, gpr: dict[Reaction, list[list[Gene
         # Obvious example: Exchange/transport reactions don't have corresponding genes in their reaction rule, so that will take a NaN-value
         enzyme_activity[rxn] = 0.0
 
-        if rxn in gpr: # ensure rxn has a gene_reaction_rule defined
+        if rxn in gpr:  # ensure rxn has a gene_reaction_rule defined
             for isozyme in gpr[rxn]:
                 # Initialize isozyme_activity for this isozyme to infinity
                 # Note: infinity-value is preserved IF this isozyme is not present in the observed transcriptomics data
@@ -86,14 +87,15 @@ def gene_expression_to_enzyme_activity(model, gpr: dict[Reaction, list[list[Gene
     return enzyme_activity
 
 
-# Function to convert transciptomics data to enzyme activity
-def convert_transcriptomics_to_enzyme_activity(transcriptomics_data: pd.DataFrame, model):#gpr: dict[Reaction, list[list[Gene]]]):
+def convert_transcriptomics_to_enzyme_activity(
+    transcriptomics_data: pd.DataFrame, model: cobra.Model
+) -> pd.DataFrame:
     """Convert transcriptomics data to enzyme activity.
 
     inputs:
         transcriptomics_data: dataframe of transcriptomics data
         model: cobra model
-        # gpr: dictionary of reactions (keys) to list of list of genes (values) for the correpsonding gene reaction rule
+        # gpr: dictionary of reaction ids (keys) to list of list of genes (values) for the correpsonding gene reaction rule
     outputs:
         enzyme_activity_df: dataframe of enzyme activity converted from transcriptomics data
     """
@@ -106,7 +108,9 @@ def convert_transcriptomics_to_enzyme_activity(transcriptomics_data: pd.DataFram
     # Loop through each strain to convert each column of transcriptomics data
     for this_strain in transcriptomics_data.columns:
         # Create dict of genes and corresponding float values using trancsciptomics data
-        expression_dict = {g: transcriptomics_data.loc[g][this_strain] for g in transcriptomics_data.index}
+        expression_dict = {
+            g: transcriptomics_data.loc[g][this_strain] for g in transcriptomics_data.index
+        }
 
         # Run the gene expression to enzyme activity converter for this_strain
         enzyme_activity_dict = gene_expression_to_enzyme_activity(model, gpr, expression_dict)
@@ -116,30 +120,37 @@ def convert_transcriptomics_to_enzyme_activity(transcriptomics_data: pd.DataFram
             # Use enzyme_activity_dict keys as the index
             enzyme_activity_df = enzyme_activity_df.reindex(enzyme_activity_dict.keys())
             # Add reaction ID column
-            enzyme_activity_df['Reaction_ID'] = [k.id for k in enzyme_activity_dict]
+            enzyme_activity_df["Reaction_ID"] = [k.id for k in enzyme_activity_dict]
 
         # Add enzymze_activity to dataframe
         enzyme_activity_df[this_strain] = enzyme_activity_dict
 
-    return enzyme_activity_df
+    if enzyme_activity_df.empty:
+        return pd.DataFrame()
 
+    return enzyme_activity_df.set_index("Reaction_ID")
+
+
+# TODO: This function and its corresponding unit test should be reviewed/rewritten.
 def prepare_data_for_bmca(all_conditions: list, measured_data: pd.DataFrame,  unmeasured_variables: list = list(), unmapped_variables: list= list()) -> pd.DataFrame:
     """prepare_data_for_bmca.
+
     all_conditions:  the full set of experimental conditions for which data is available.
     measured_data: a DataFrame that contains measurements of variables in at least one experimental condition.  rows are variable names and columns are experimental conditions.
     unmapped_variables: a list of variables (metabolite_ids or reaction ids) that is unobservable and therefore cannot be mapped to data. (exchange reactions that have no enzyme)
     unmeasured_variables: a list of variable (metabolite_ids or reaction ids) that are not measured in any condition
     """
-    # Get the set of all variables
-    all_variables = sorted(set(measured_data.index).union(set(unmeasured_variables)).union(set(unmapped_variables)))
-    # Get the set of all conditions
-    all_conditions = sorted(set(all_conditions))
-    # Create a DataFrame to hold the data
-    data = pd.DataFrame(np.Inf, index=all_variables, columns=all_conditions)
-    # Fill in the measured data
-    data.loc[measured_data.index, measured_data.columns] = measured_data
-    # Fill in the unmeasured data
-    data.loc[unmeasured_variables, :] = np.Inf
-    # Fill in  unmapped data
-    data.loc[unmapped_variables, :] = np.nan
-    return data
+    # # Get the set of all variables
+    # all_variables = sorted(set(measured_data.index).union(set(unmeasured_variables)).union(set(unmapped_variables)))
+    # # Get the set of all conditions
+    # all_conditions = sorted(set(all_conditions))
+    # # Create a DataFrame to hold the data
+    # data = pd.DataFrame(np.Inf, index=all_variables, columns=all_conditions)
+    # # Fill in the measured data
+    # data.loc[measured_data.index, measured_data.columns] = measured_data
+    # # Fill in the unmeasured data
+    # data.loc[unmeasured_variables, :] = np.Inf
+    # # Fill in  unmapped data
+    # data.loc[unmapped_variables, :] = np.nan
+    # return data
+    return pd.DataFrame()
